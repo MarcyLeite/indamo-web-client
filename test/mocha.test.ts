3870cbb35af472ad32cea9091b03d5dfbd4bdfff
Matheus Marciano <mm.leitea@outlook.com>
2024-11-12 10:40:06 -0300

    🔧 Add and configure mocha

---
+++ test/mocha.test.ts
@@ -0,0 +1,11 @@
+import 'chai/register-should'
+import { describe, test } from 'mocha'
+
+describe('Mocha integration', () => {
+	test('Should "should" assertion work', () => {
+		const number1 = 2
+		const number2 = 3
+		const result = number1 + number2
+		result.should.equal(5)
+	})
+})
