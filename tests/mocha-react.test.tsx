5d9c5fb355698ff414772e81f350f49b85154cc5
Matheus Marciano <mm.leitea@outlook.com>
2024-11-13 11:17:56 -0300

    🧑‍💻 Add globals to mocha test env

--- tests/mocha-react.test.tsx
+++ tests/mocha-react.test.tsx
@@ -1,17 +1,9 @@
-import React from 'react'
-
 import { render, fireEvent, screen } from '@testing-library/react'
-import { describe, test } from 'mocha'
-import 'chai/register-should'
 
 describe('Mocha with React and testing-library integration', () => {
-	test('Should createand render button and trigger click event', () => {
+	it('Should createand render button and trigger click event', () => {
 		render(
-			React.createElement(() => (
-				<button onClick={(e) => ((e.target as HTMLButtonElement).innerHTML = 'B')}>
-					A
-				</button>
-			))
+			<button onClick={(e) => ((e.target as HTMLButtonElement).innerHTML = 'B')}>A</button>
 		)
 
 		const element = screen.getByText('A')
