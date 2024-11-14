8cbfdfad8e1ba92594683eedcead435e9dcc2c63
Matheus Marciano <mm.leitea@outlook.com>
2024-11-14 13:15:39 -0300

    🚨 Format test

--- tests/mocha-react.test.tsx
+++ tests/mocha-react.test.tsx
@@ -2,9 +2,7 @@ import { render, fireEvent, screen } from '@testing-library/react'
 
 describe('Mocha with React and testing-library integration', () => {
 	it('Should createand render button and trigger click event', () => {
-		render(
-			<button onClick={(e) => ((e.target as HTMLButtonElement).innerHTML = 'B')}>A</button>
-		)
+		render(<button onClick={(e) => ((e.target as HTMLButtonElement).innerHTML = 'B')}>A</button>)
 
 		const element = screen.getByText('A')
 		fireEvent.click(element)
