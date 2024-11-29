55dcda8e53892c42082683d3c6e4203d64686d05
Matheus Marciano <mm.leitea@outlook.com>
2024-11-29 15:56:54 -0300

    🐛 Fix app main with view config

--- src/Indamo.tsx
+++ src/Indamo.tsx
@@ -2,9 +2,11 @@ import axios from 'axios'
 import { useLoader } from '@react-three/fiber'
 import { GLTFLoader } from 'three/examples/jsm/Addons.js'
 import { useEffect, useState } from 'react'
-import { ViewConfig } from './modules/views'
+
 import Scene3D from './components/threejs/Scene3D'
-import InteractableObject from './components/threejs/InteractableObject'
+import InteractableObject, { PaintMap } from './components/threejs/InteractableObject'
+import { createViewController } from './modules/views/controller'
+import { ViewConfig } from './modules/views/factory'
 
 // FIXME type does not belong here. Maybe When creating move to a related database connection.
 export type IndamoData = {
@@ -15,24 +17,59 @@ export type IndamoData = {
 	raw: number
 }
 
+export type IndamoConfig = {
+	views: ViewConfig[]
+}
+
 const Indamo = () => {
 	const model = useLoader(GLTFLoader, 'snowman.glb')
-	const [viewConfig, setViewConfig] = useState<ViewConfig | null>(null)
+	const [config, setConfig] = useState<IndamoConfig | null>(null)
+	const [paintMap, setPaintMap] = useState<PaintMap>({})
 
 	const fetchViewConfig = async () => {
-		const response = await axios.get('http://localhost:5173/thermal-view-demo.json')
-		setViewConfig(response.data)
+		const response = await axios.get('http://localhost:5173/app-config-demo.json')
+		setConfig(response.data)
 	}
 
 	useEffect(() => {
 		fetchViewConfig()
 	}, [])
 
-	useEffect(() => {}, [viewConfig])
+	useEffect(() => {
+		if (!config) return
+		const viewController = createViewController(config.views)
+		const view = viewController.getViewById('thermal-demo')
+
+		const _paintMap = view!.createPaintMap({
+			A: {
+				measurement: 'A',
+				source: '',
+				status: '',
+				raw: 0,
+				eng: 0,
+			},
+			B: {
+				measurement: 'B',
+				source: '',
+				status: '',
+				raw: 50,
+				eng: 50,
+			},
+			C: {
+				measurement: 'C',
+				source: '',
+				status: '',
+				raw: 100,
+				eng: 100,
+			},
+		})
+
+		setPaintMap(_paintMap)
+	}, [config])
 
 	return (
 		<Scene3D>
-			<InteractableObject object3d={model.scene} paintMap={{}}></InteractableObject>
+			<InteractableObject object3d={model.scene} paintMap={paintMap}></InteractableObject>
 		</Scene3D>
 	)
 }
