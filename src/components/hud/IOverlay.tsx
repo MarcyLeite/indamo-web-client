ffd5ee80e4419cc5b53d10e15029a9f68929302d
Matheus Marciano <mm.leitea@outlook.com>
2024-12-04 14:54:46 -0300

    💄 Upgrade composition scss api + move all scss files to assets/styles

--- src/components/hud/IOverlay.tsx
+++ src/components/hud/IOverlay.tsx
@@ -1,14 +1,20 @@
 import { ReactNode } from 'react'
 
 type Props = {
-	topRight: ReactNode
+	topRight?: ReactNode
+	topLeft?: ReactNode
 }
 
-const IOverlay = ({ topRight }: Props) => {
+const IOverlay = ({ topRight, topLeft }: Props) => {
 	return (
 		<div className="i-overlay p-absolute w-100 h-100 top-0 pointer-events-none">
 			<div className="p-relative w-100 h-100">
-				<div className="i-overlay-element w-fit-content i-overlay-top-right pointer-events-all">{topRight}</div>
+				<div className="i-overlay-element w-fit-content i-overlay-top-left pointer-events-all">
+					{topLeft}
+				</div>
+				<div className="i-overlay-element w-fit-content i-overlay-top-right pointer-events-all">
+					{topRight}
+				</div>
 			</div>
 		</div>
 	)
