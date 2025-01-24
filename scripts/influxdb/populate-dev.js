12090672874d67cef574da2b5c5df1ff04139d41
Matheus Marciano <mm.leitea@outlook.com>
2025-01-24 12:21:21 -0300

    🧑‍💻 Add scripts to setup the dev env and update populate script

--- scripts/influxdb/populate-dev.js
+++ scripts/influxdb/populate-dev.js
@@ -1,41 +1,66 @@
 import { InfluxDB, Point } from '@influxdata/influxdb-client'
-import { readFileSync } from 'fs'
+import dotenv from 'dotenv'
+import axios from 'axios'
+dotenv.config()
 
-const influxConfig = readFileSync('./.influx/config/influx-configs').toString('utf-8').split('\n')
-const token = influxConfig[2].match(/"([^"]*)"/)[0].replaceAll('"', '')
+const { VITE_INFLUX_URL, VITE_INFLUX_TOKEN, VITE_INFLUX_ORG, VITE_INFLUX_BUCKET, VITE_DEV_DATE } =
+	process.env
 
-const influxDB = new InfluxDB({
-	url: 'http://localhost:8086',
-	token,
-})
+const getWriteApi = () =>
+	new Promise((resolve) => {
+		const influxDB = new InfluxDB({
+			url: VITE_INFLUX_URL,
+			token: VITE_INFLUX_TOKEN,
+		})
+		const writeApi = influxDB.getWriteApi(VITE_INFLUX_ORG, VITE_INFLUX_BUCKET)
 
-const writeApi = influxDB.getWriteApi('dev', 'dev')
-const now = Date.now()
-const pointCount = 100
-for (let i = 0; i < pointCount; i++) {
-	const timestamp = (now - (pointCount - i) * 1000) * 1000000
-	const pointA = new Point('A')
-		.tag('source', 'ME')
-		.tag('status', 'GOOD')
-		.floatField('raw', i)
-		.floatField('eng', i % 12)
-		.timestamp(timestamp)
+		const request = async () => {
+			try {
+				await axios.get(VITE_INFLUX_URL)
+				resolve(writeApi)
+			} catch {
+				setTimeout(request, 1000)
+			}
+		}
+		request()
+	})
 
-	const pointB = new Point('B')
-		.tag('source', 'ME')
-		.tag('status', 'BAD')
-		.floatField('raw', i)
-		.floatField('eng', i + 1000)
-		.timestamp(timestamp)
+const populateDev = async () => {
+	const now = Date.parse(VITE_DEV_DATE)
+	const writeApi = await getWriteApi()
 
-	const pointC = new Point('C')
-		.tag('source', 'OTHER')
-		.tag('status', i % 2 === 0 ? 'GOOD' : 'BAD')
-		.floatField('raw', i * 4)
-		.floatField('eng', i)
-		.timestamp(timestamp)
+	const pointCount = 100
+	for (let i = 0; i < pointCount; i++) {
+		const increase = i * 1000
+		const timestamp = now + increase
+		const datetime = new Date(timestamp)
 
-	writeApi.writePoints([pointA, pointB, pointC])
+		const pointA = new Point('A')
+			.tag('source', 'ME')
+			.tag('status', 'GOOD')
+			.floatField('raw', i)
+			.floatField('eng', i % 12)
+			.timestamp(datetime)
+
+		const pointB = new Point('B')
+			.tag('source', 'ME')
+			.tag('status', 'BAD')
+			.floatField('raw', i)
+			.floatField('eng', i + 1000)
+			.timestamp(datetime)
+
+		const pointC = new Point('C')
+			.tag('source', 'OTHER')
+			.tag('status', i % 2 === 0 ? 'GOOD' : 'BAD')
+			.floatField('raw', i * 4)
+			.floatField('eng', i)
+			.timestamp(datetime)
+
+		writeApi.writePoints([pointA, pointB, pointC])
+		await writeApi.flush()
+		console.log(`Point added at: ${datetime}`)
+	}
+	console.log('Done!')
 }
 
-writeApi.close().then(() => console.log('Done!'))
+populateDev()
