4be57c5b775a592366964bc8dc7703e8d7eefe0c
Matheus Marciano <mm.leitea@outlook.com>
2025-02-18 11:10:00 -0300

    ♻️ Readd component info

---
+++ src/components/ui/ComponentPanel.tsx
@@ -0,0 +1,74 @@
+import { useEffect, useState } from 'react'
+import { IndamoData } from '../../modules/consumer/connection'
+import { IndamoComponentConfig } from '../../modules/views/factory'
+import IPanel from '../hud/IPanel'
+import IExpandPanel from '../hud/IExpandPanel'
+import ITable from '../hud/ITable'
+import { PropsWithIndamoStore } from '../../store'
+
+const ComponentPanel = ({
+	store: {
+		view,
+		dataMap,
+		scene: { selected },
+	},
+}: PropsWithIndamoStore) => {
+	const [config, setConfig] = useState<IndamoComponentConfig | null>(null)
+	const [data, setData] = useState<IndamoData[] | null>(null)
+
+	useEffect(() => {
+		const _config = view?.getComponentConfig(selected?.id) ?? null
+		setConfig(_config)
+		const _data =
+			_config?.dataIndexers?.map((indexer) => {
+				const value = Object.assign({}, dataMap[indexer])
+
+				value.index = indexer
+				delete value._measurement
+				delete value._start
+				delete value._stop
+				delete value._time
+				delete value.result
+				delete value.table
+				return value
+			}) ?? null
+
+		setData(_data)
+	}, [view, selected, dataMap])
+
+	return (
+		<div className="ma-4">
+			<IPanel>
+				<div className="pa-4">
+					<IExpandPanel
+						className="ga-4"
+						title={
+							!selected
+								? 'Component Info'
+								: !config || config.display === undefined
+									? selected.name
+									: config.display
+						}
+					>
+						{!selected || !view ? null : !config ? (
+							<div>Add</div>
+						) : (
+							<div className="d-flex flex-column ga-2">
+								<div>
+									<span>
+										Eng:{' '}
+										<span className="text-bold">{data ? (data[0]?.eng ?? 'None') : 'None'}</span>
+									</span>
+								</div>
+								<IExpandPanel className="ga-2" title="Map" hasSeparator={false}>
+									{<ITable dataList={data ?? []} order={['index']} />}
+								</IExpandPanel>
+							</div>
+						)}
+					</IExpandPanel>
+				</div>
+			</IPanel>
+		</div>
+	)
+}
+export default ComponentPanel
--- src/store.ts
+++ src/store.ts
@@ -1,6 +1,11 @@
+import { useEffect, useState } from 'react'
 import { useConfiguration } from './modules/configuration/hook'
+import { IndamoConnection } from './modules/consumer/connection'
 import { useTimeControl } from './modules/time-control/hook'
 import { useView } from './modules/views/hook'
+import { createInfluxConnection } from './modules/consumer/influx-connection'
+import { useConsumer } from './modules/consumer'
+import { Object3D } from 'three'
 
 export const useIndamoStore = () => {
 	const configuration = useConfiguration(`${window.location.origin}/indamo-config.json`)
@@ -9,7 +14,31 @@ export const useIndamoStore = () => {
 	const initialDate = import.meta.env?.DEV ? new Date(import.meta.env.VITE_DEV_DATE) : new Date()
 	const timeControl = useTimeControl(initialDate)
 
-	return { ...viewHook, configuration, timeControl }
+	const [connection, setConnection] = useState<IndamoConnection | null>(null)
+
+	const consumer = useConsumer(timeControl.moment, viewHook.view, connection)
+
+	useEffect(() => {
+		if (!configuration.connection) {
+			setConnection(null)
+			return
+		}
+		setConnection(createInfluxConnection(configuration.connection.options))
+	}, [configuration.connection])
+
+	const [component, setComponent] = useState<Object3D | null>(null)
+
+	return {
+		...viewHook,
+		configuration,
+		timeControl,
+		dataMap: consumer.dataMap,
+		consumer,
+		scene: {
+			selected: component,
+			setSelected: setComponent,
+		},
+	}
 }
 
 export type IndamoStore = ReturnType<typeof useIndamoStore>
